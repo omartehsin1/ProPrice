15a6715a4a81729abd9cb2f5686930758a0a7440
Preyansh <preyansh98@gmail.com>
2019-08-25 00:19:17 -0400

    gcp to key

--- src/main/java/com/hackthe6ix/proprice/utils/GCPConstants.java
+++ src/main/java/com/hackthe6ix/proprice/utils/GCPConstants.java
@@ -3,8 +3,6 @@ package com.hackthe6ix.proprice.utils;
 public class GCPConstants {
 
     //Static Config
-    public final static String GCP_IDENTIFIER_MS_ENDPOINT = "";
-
-
+    public final static String GCP_IDENTIFIER_MS_ENDPOINT = System.getenv("GCP_IDENTIFIER_MS_ENDPOINT");
 
 }
